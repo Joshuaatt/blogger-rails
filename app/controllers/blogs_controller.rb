d5bca663acf0c4d8ea4ea3fbac22411b4a7e539a
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-03-27 17:35:37 -0700

    refactor blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -39,8 +39,6 @@ class BlogsController < ApplicationController
 
   def destroy
     @blog = Blog.find(params[:id])
-    @comments = @blog.comments.find(params[:id])
-    @blog.comments.destroy
     @blog.destroy
     flash[:notice] = "Your blog has been deleted"
     redirect_to blogs_path
